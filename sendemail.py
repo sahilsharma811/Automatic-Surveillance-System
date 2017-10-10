fffdbaf0b8607cdf1f9482147bafbe0ed93a467a
sahilsharma811 <sahil8sharma8@gmail.com>
2017-10-10 15:08:15 +0530

    Modularity in Code

--- sendemail.py
+++ sendemail.py
@@ -1,51 +1,42 @@
-from surveillance import out
-from graphplot import p
-
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
-sender = 'Sender Email Address'
-receivers = ['reciever1' , 'reciever2']
+def send_email(sender, password,receivers='sahil8sharma8@gmail.com', subject='Alert! Intrution Detected'):
+	COMMASPACE = ', '
+	msg = MIMEMultipart()
+	msg['From'] = sender
+	msg['To'] = COMMASPACE.join(receivers)
+	msg['Subject'] = subject
+	body = "Someone just entered your premises. Take Quick Actions!"
 
-msg = MIMEMultipart()
+	msg.attach(MIMEText(body, 'plain'))
 
-COMMASPACE = ', '
+	# Attaching Video
+	filename = "video.mp4"
+	attachment1 = open("output.mp4", "rb")
+	part = MIMEBase('application', 'octet-stream')
+	part.set_payload((attachment1).read())
+	encoders.encode_base64(part)
+	part.add_header('Content-Disposition', "attachment1; filename= %s" % filename)
+	msg.attach(part)
 
-msg['From'] = sender
-msg['To'] = COMMASPACE.join(receivers)
-
-msg['Subject'] = "Alert! Intrution Detected"
-body = "Someone just entered your premises. Take Quick Actions!"
-
-msg.attach(MIMEText(body, 'plain'))
-
-# Attaching Video
-filename = "video.mp4"
-attachment1 = open("video.mp4", "rb")
-part = MIMEBase('application', 'octet-stream')
-part.set_payload((attachment1).read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition', "attachment1; filename= %s" % filename)
-
-msg.attach(part)
-
-# Attaching graph
-filename = "Graph.html"
-attachment2 = open("Graph.html", "rb")
-part1 = MIMEBase('application', 'octet-stream')
-part1.set_payload((attachment2).read())
-encoders.encode_base64(part1)
-part1.add_header('Content-Disposition', "attachment2; filename= %s" % filename)
-
-msg.attach(part1)
-
-# Configuring Server
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(sender, "password_here")
-text = msg.as_string()
-server.sendmail(sender, receivers, text)
-server.quit()
+	# Attaching graph
+	'''
+	filename = "Graph.html"
+	attachment2 = open("Graph.html", "rb")
+	part1 = MIMEBase('application', 'octet-stream')
+	part1.set_payload((attachment2).read())
+	encoders.encode_base64(part1)
+	part1.add_header('Content-Disposition', "attachment2; filename= %s" % filename)
+	msg.attach(part1)
+	'''
+	# Configuring Server
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.starttls()
+	server.login(sender, password)
+	text = msg.as_string()
+	server.sendmail(sender, receivers, text)
+	server.quit()
--- surveillance.py
+++ surveillance.py
@@ -1,57 +1,70 @@
 import numpy as np
 import cv2, time, pandas
 from datetime import datetime
+import getpass
+from sendemail import send_email
 
-face_cascade = cv2.CascadeClassifier('frontalface_cascade.xml')
-eye_cascade = cv2.CascadeClassifier('eye_cascade.xml')
+def start_surveillance(sender,receives,password):
 
-#Initializations for graph
-first_frame=None
-status_list=[None,None]
-times=[]
-df=pandas.DataFrame(columns=["Start","End"])
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+    
+    #Initializations for graph
+    status_list=[None,None]
+    times=[]
+    df=pandas.DataFrame(columns=["Start","End"])
 
-# Initializations for saving the video!
-fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-out = cv2.VideoWriter('output1.mp4',fourcc, 15.0, (640,480))
-cap = cv2.VideoCapture()
-cap.open(0)
+    # Initializations for saving the video!
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    out = cv2.VideoWriter('output.mp4',fourcc, 15.0, (640,480))
+    cap = cv2.VideoCapture()
+    cap.open(0)
+    counter = 0
+    
+    while 1:
+        status=0
+        ret, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        face = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w,h) in face:
+            status=1
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),3)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
 
-while 1:
-    status=0
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    face = face_cascade.detectMultiScale(gray, 1.3, 5)
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),1)
+            out.write(img)
 
-    for (x,y,w,h) in face:
-        status=1
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),3)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
+        status_list.append(status)
+        status_list=status_list[-2:]
 
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),1)
-        out.write(img)
-
-    status_list.append(status)
-    status_list=status_list[-2:]
-
-    if status_list[-1]==1 and status_list[-2]==0:
-        times.append(datetime.now())
-    if status_list[-1]==0 and status_list[-2]==1:
-        times.append(datetime.now())
-
-    cv2.imshow('img',img)
-    k = cv2.waitKey(1)
-    if k==ord('q'):
-        if status==1:
+        if status_list[-1]==1 and status_list[-2]==0:
             times.append(datetime.now())
-        break
+        if status_list[-1]==0 and status_list[-2]==1:
+            times.append(datetime.now())
+
+        cv2.imshow('img',img)
+        k = cv2.waitKey(1)
+        if k==ord('q'):
+            if status==1:
+                times.append(datetime.now())
+            break
         
-for i in range(0,len(times),2):
-    df=df.append({"Start":times[i],"End":times[i+1]},ignore_index=True)
-df.to_csv("Times.csv")
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+    for i in range(0,len(times),2):
+        df=df.append({"Start":times[i],"End":times[i+1]},ignore_index=True)
+    df.to_csv("Times.csv")
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+    send_email(sender,password,receivers,'First Alert')
+
+
+if __name__ == '__main__':
+    sender = 'automaticsurveillance@gmail.com'
+    #receivers=input('Enter the Receiver Email_address: ')
+    receivers = ['sahil8sharma8@gmail.com', 'sneh.rathore96@gmail.com', 'parthbhasin96@gmail.com']
+    password = getpass.getpass("Password:")
+    start_surveillance(sender,receivers,password)  
+    
