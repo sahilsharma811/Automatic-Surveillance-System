a2954400d48adfc1523114f5e29beb503fe9f83d
sahilsharma811 <sahil8sharma8@gmail.com>
2017-10-15 19:36:26 +0530

    Sends Snapshots Intruder

--- sendemail.py
+++ sendemail.py
@@ -3,40 +3,36 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+class email():
+	def configure(self,sender, password):
+		self.server = smtplib.SMTP('smtp.gmail.com', 587)
+		self.server.starttls()
+		self.server.login(sender, password)
 
-def send_email(sender, password,receivers='sahil8sharma8@gmail.com', subject='Alert! Intrution Detected'):
-	COMMASPACE = ', '
-	msg = MIMEMultipart()
-	msg['From'] = sender
-	msg['To'] = COMMASPACE.join(receivers)
-	msg['Subject'] = subject
-	body = "Someone just entered your premises. Take Quick Actions!"
+	def send_email(self,sender,argv,receivers='sahil8sharma8@gmail.com', subject='Alert! Intrution Detected'):
+		COMMASPACE = ', '
+		msg = MIMEMultipart()
+		msg['From'] = sender
+		msg['To'] = COMMASPACE.join(receivers)
+		msg['Subject'] = subject
+		body = "Someone just entered your premises. Take Quick Actions!"
 
-	msg.attach(MIMEText(body, 'plain'))
+		msg.attach(MIMEText(body, 'plain'))
 
 	# Attaching Video
-	filename = "video.mp4"
-	attachment1 = open("output.mp4", "rb")
-	part = MIMEBase('application', 'octet-stream')
-	part.set_payload((attachment1).read())
-	encoders.encode_base64(part)
-	part.add_header('Content-Disposition', "attachment1; filename= %s" % filename)
-	msg.attach(part)
+		for arg in argv:
+			filename = arg
+			attachment = open(arg, "rb")
+			part = MIMEBase('application', 'octet-stream')
+			part.set_payload((attachment).read())
+			encoders.encode_base64(part)
+			part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+			msg.attach(part)
 
-	# Attaching graph
-	'''
-	filename = "Graph.html"
-	attachment2 = open("Graph.html", "rb")
-	part1 = MIMEBase('application', 'octet-stream')
-	part1.set_payload((attachment2).read())
-	encoders.encode_base64(part1)
-	part1.add_header('Content-Disposition', "attachment2; filename= %s" % filename)
-	msg.attach(part1)
-	'''
 	# Configuring Server
-	server = smtplib.SMTP('smtp.gmail.com', 587)
-	server.starttls()
-	server.login(sender, password)
-	text = msg.as_string()
-	server.sendmail(sender, receivers, text)
-	server.quit()
+		text = msg.as_string()
+		self.server.sendmail(sender, receivers, text)
+		#server.quit()
+if __name__=='__main__':
+	obj = email()
+	obj.configure()
--- surveillance.py
+++ surveillance.py
@@ -2,9 +2,11 @@ import numpy as np
 import cv2, time, pandas
 from datetime import datetime
 import getpass
-from sendemail import send_email
+from sendemail import email
 
 def start_surveillance(sender,receives,password):
+    mail_obj = email()
+    mail_obj.configure(sender,password)
 
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
@@ -19,33 +21,43 @@ def start_surveillance(sender,receives,password):
     out = cv2.VideoWriter('output.mp4',fourcc, 15.0, (640,480))
     cap = cv2.VideoCapture()
     cap.open(0)
-    counter = 0
-    
+    count = 0
+    lst = []
     while 1:
         status=0
         ret, img = cap.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         face = face_cascade.detectMultiScale(gray, 1.3, 5)
+        print(face)
         for (x,y,w,h) in face:
             status=1
             cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),3)
             roi_gray = gray[y:y+h, x:x+w]
             roi_color = img[y:y+h, x:x+w]
-
             eyes = eye_cascade.detectMultiScale(roi_gray)
             for (ex,ey,ew,eh) in eyes:
                 cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),1)
-            out.write(img)
+            out.write(img) 
+            cv2.imwrite("frame%d.jpg" % count, img)
+            count+=1
+
+        if status!=0:
+            for i in range(count):
+                lst.append('frame'+str(i)+'.jpg')
+            print(lst)
+            mail_obj.send_email(sender,lst,receivers,'Alert!!')             
 
         status_list.append(status)
         status_list=status_list[-2:]
 
         if status_list[-1]==1 and status_list[-2]==0:
             times.append(datetime.now())
+            
         if status_list[-1]==0 and status_list[-2]==1:
             times.append(datetime.now())
 
         cv2.imshow('img',img)
+
         k = cv2.waitKey(1)
         if k==ord('q'):
             if status==1:
@@ -58,12 +70,12 @@ def start_surveillance(sender,receives,password):
     cap.release()
     out.release()
     cv2.destroyAllWindows()
-    send_email(sender,password,receivers,'First Alert')
+    #send_email(sender,password,receivers,'First Alert')
 
 
 if __name__ == '__main__':
-    sender = 'sender email address'
-    receivers = ['receives1@gmail.com', 'receivers2@gmail.com']
+    sender = 'sender_email_address'
+    receivers = ['sahil8sharma8@gmail.com','receiver_email_address','receiver_email_address']
     password = getpass.getpass("Password:") # Sender's Password
     start_surveillance(sender,receivers,password)  
     
